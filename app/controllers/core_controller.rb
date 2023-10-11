2a862aad38a9794c2ddbac9bd9a38ba6cc2a477e
tayyab2914 <tayyab1234rehman@gmail.com>
2023-10-11 17:41:56 +0500

    Added Dashboard

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -1,4 +1,8 @@
 class CoreController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
+  end
+
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'core#index'
   devise_for :users
+  get 'dashboard', to: "core#dashboard" 
 end
